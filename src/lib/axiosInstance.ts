3bb7c1ab1648d1754f41423550d6f38d4e3a8af5
naimuddin94 <developernaimuddin@gmail.com>
2024-10-11 21:45:23 +0600

    axios instance create

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,36 @@
+import { envConfig } from "@/config";
+import axios from "axios";
+import { cookies } from "next/headers";
+
+const axiosInstance = axios.create({
+  baseURL: envConfig.api_host,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const cookieStore = cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
+
+    if (accessToken) {
+      if (config.headers) {
+        config.headers["cookie"] = `accessToken=${accessToken}`;
+      } else {
+        (config as any).headers = {
+          cookie: `accessToken=${accessToken}`,
+        };
+      }
+    }
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(function (response) {
+  return response;
+});
+
+export default axiosInstance;
