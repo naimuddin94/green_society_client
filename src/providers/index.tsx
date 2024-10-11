d23aed27e5b2288b32ebe032b4a7b9f7bd219311
naimuddin94 <developernaimuddin@gmail.com>
2024-10-11 22:25:51 +0600

    all provider setup

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,20 @@
-// 1. import `NextUIProvider` component
+"use client";
+
+import UserProvider from "@/context/user.provider";
 import { IChildrenProps } from "@/types";
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: IChildrenProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <Toaster />
+        <NextUIProvider>{children}</NextUIProvider>
+      </UserProvider>
+    </QueryClientProvider>
+  );
 }
